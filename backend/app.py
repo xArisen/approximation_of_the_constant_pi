1ec2532e87c4a278bc021f97f5b305bd66655e25
xArisen <czapiewskipiotr04@gmail.com>
2021-04-06 21:40:38 +0200

    Added: Basic implementation of calculating figure field (created by chart) aproximation | Changed: controllers URL

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import jsonify
+import json
 from flask import request
 from utils.monte_carlo import approximation_of_the_PI
+from utils.monte_carlo import approximation_of_the_figure_field_created_by_chart
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -13,10 +15,25 @@ def test():
     return jsonify(result='Hello')
 
 
-@app.route('/monte_carlo', methods=['GET'])
-def test_post():
+@app.route('/monte_carlo/approximation_of_the_PI', methods=['GET'])
+def calculate1():
     runs = request.args.get('runs')
     result = approximation_of_the_PI(int(runs))
+
+    return jsonify(result)
+
+
+# Delete after dev
+exapleJson = '{ "points_coordinates": [ [0, 0], [2, 2],  [6, 1], [8, 0] ] }'
+
+# TODO change for external API
+
+
+@app.route('/monte_carlo/approximation_of_the_figure_field', methods=['GET'])
+def calculate2():
+    runs = request.args.get('runs')
+    result = approximation_of_the_figure_field_created_by_chart(
+        json.loads(exapleJson)['points_coordinates'], int(runs))
 
     return jsonify(result)
 
--- backend/utils/monte_carlo.py
+++ backend/utils/monte_carlo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from decimal import Decimal
 
 
 def approximation_of_the_PI(runs):
@@ -8,6 +9,7 @@ def approximation_of_the_PI(runs):
     times = 10
     pi = 0
     lim = pow(0.5, 2)
+    surface_area_of_lim_figure = 4
     for i in range(0, times):
         in_circle = 0
 
@@ -18,6 +20,60 @@ def approximation_of_the_PI(runs):
             if f <= lim:
                 in_circle += 1
 
-        pi += (in_circle/runs)*4
+        pi += (in_circle/runs)*surface_area_of_lim_figure
 
     return pi/times
+
+# TODO RESTRUCTURE
+
+
+def approximation_of_the_figure_field_created_by_chart(coordinates_of_points, runs):
+    if(runs > 1_000_000):
+        return 0
+
+    ab = list()
+    max_x = 0
+    max_y = 0
+    for i in range(0, len(coordinates_of_points)):
+        if i < len(coordinates_of_points)-1:
+            ab.append(
+                lin_equ(coordinates_of_points[i], coordinates_of_points[i+1])
+            )
+
+        if max_x < coordinates_of_points[i][0]:
+            max_x = coordinates_of_points[i][0]
+
+        if max_y < coordinates_of_points[i][1]:
+            max_y = coordinates_of_points[i][1]
+
+    in_figure = 0
+    for i in range(0, runs):
+        xs = np.random.uniform(0, max_x, 1)[0]
+        ys = np.random.uniform(0, max_y, 1)[0]
+        iteration = check_point_range(xs, ys, coordinates_of_points)
+        lim_y = calculate_lim_for_x(xs, ab[iteration])
+        if ys <= lim_y:
+            in_figure += 1
+
+    surface_area_of_lim_figure = max_x*max_y
+    surface_area_of_figure = (in_figure/runs)*surface_area_of_lim_figure
+
+    return surface_area_of_figure
+
+
+def lin_equ(point1, point2):
+    a = (point2[1] - point1[1]) / (point2[0] - point1[0])
+    b = (point2[1] - (a * point2[0]))
+
+    return a, b
+
+
+def check_point_range(x, y, coordinates_of_points):
+    for i in range(0, len(coordinates_of_points)):
+        if coordinates_of_points[i][0] <= x and coordinates_of_points[i+1][0] >= x:
+            return i
+
+
+def calculate_lim_for_x(x, ab):
+    y = ab[0]*x + ab[1]
+    return y
